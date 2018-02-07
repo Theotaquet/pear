87168c45d82816e2845d0d3b96b5c3c6ebcf42ee
Theotaquet <to.constant@gmail.com>
2018-02-07 18:13:06 +0100

    Add PearAnalyser.cs and the related classes
    Add the Unity plugin to analyse game perfs, and add the Session and the Metric classes in order to create serializable objects.

---
+++ plugin/Metric.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace AssemblyCSharp {
+
+	//performance data recorded during a session
+	[Serializable]
+	public class Metric {
+
+		//the type of metric (fps, frametime, ...)
+		public string type;
+		//the recorded value
+		public int value;
+		//the time the metric was recorded
+		public uint time;
+
+		public Metric(string type, int value, uint time) {
+			this.type = type;
+			this.value = value;
+			this.time = time;
+		}
+
+		public override string ToString() {
+			return time + " - " + type + ": " + value;
+		}
+	}
+}
