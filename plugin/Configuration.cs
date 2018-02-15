c0e0f3e402db58db14de787358aebcac998b5262
Theotaquet <to.constant@gmail.com>
2018-02-15 14:59:06 +0100

    Add configuration file system
    Configuration properties are now stored in a .ini file and are saved when starting the plugin

---
+++ plugin/Configuration.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using IniParser;
+using IniParser.Model;
+
+namespace Pear {
+
+    public class Configuration : MonoBehaviour {
+
+        public static string ServerURL;
+
+        public static bool FpsEnabled;
+        public static float UpdateFrequency;
+
+        void Awake() {
+            LoadConfig();
+        }
+
+        public static void LoadConfig() {
+            FileIniDataParser parser = new FileIniDataParser();
+            IniData data = parser.ReadFile("Assets/pear/config.ini");
+
+            ServerURL = data["ServerConfiguration"]["serverURL"];
+
+            FpsEnabled = bool.Parse(data["FpsConfiguration"]["fpsEnabled"]);
+            UpdateFrequency = float.Parse(data["FpsConfiguration"]["updateFrequency"]) / 1000;
+        }
+    }
+}
