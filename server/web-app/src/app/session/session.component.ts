35ae3754fa0018597e556b323eba4f8f45236367
Theotaquet <to.constant@gmail.com>
2018-05-11 16:58:28 +0200

    Use the JavaScript literal notation and add options

--- server/web-app/src/app/session/session.component.ts
+++ server/web-app/src/app/session/session.component.ts
@@ -34,51 +34,132 @@ export class SessionComponent implements OnInit {
   }
 
   loadCharts() {
-    for(let metricsManager of this.session.metricsManagers) {
-      for(let statistic of metricsManager.statistics) {
+    for(const metricsManager of this.session.metricsManagers) {
+      for(const statistic of metricsManager.statistics) {
         if(statistic.thresholds) {
-          google.charts.load('current', {packages: ['corechart']});
+          google.charts.load('current', { packages: ['corechart'] });
           google.charts.setOnLoadCallback(() => this.createChart(metricsManager, statistic));
         }
       }
     }
   }
 
+//   createChart(metricsManager, statistic): any {
+//     var thresholds = statistic.thresholds;
+//     var metricName = this.formatMetricsManagerNamePipe.transform(metricsManager.name);
+//     var chartName = this.formatChartNamePipe.transform(metricsManager.name, statistic.name);
+
+//     var data = new google.visualization.DataTable();
+//     data.addColumn('number', 'x');
+//     data.addColumn('number', metricName);
+//     data.addColumn( {'type': 'string', 'role': 'style'} );
+//     data.addColumn( {'type': 'string', 'role': 'annotation'} );
+
+//     var minFound = false;
+//     var maxFound = false;
+
+//     for(let metric of metricsManager.metrics) {
+//       if(metric.value >= thresholds.minimum && metric.value <= thresholds.maximum) {
+//         var color = '#ccff00';
+//       }
+//       else {
+//         var color = '#ff3f00';
+//       }
+
+//       var annotation = null;
+//       if(!minFound && metric.value == metricsManager.statistics.find(x => x.name == 'minimum')) {
+//         annotation = 'min';
+//         minFound = true;
+//       }
+//       else if(!maxFound && metric.value == metricsManager.statistics.find(x => x.name = 'maximum')) {
+//         annotation = 'max';
+//         maxFound = true;
+//       }
+//       data.addRow([metric.recordTime, metric.value, `{fill-color: ${color}}`, annotation]);
+//     }
+
+//     var chart = new google.visualization.LineChart(document.getElementById(chartName));
+//     chart.draw(data, null);
+//   }
+
   createChart(metricsManager, statistic): any {
-    var thresholds = statistic.thresholds;
-    var metricName = this.formatMetricsManagerNamePipe.transform(metricsManager.name);
-    var chartName = this.formatChartNamePipe.transform(metricsManager.name, statistic.name);
+    const thresholds = statistic.thresholds;
+    const metricName = this.formatMetricsManagerNamePipe.transform(metricsManager.name);
+    const chartName = this.formatChartNamePipe.transform(metricsManager.name, statistic.name);
 
-    var data = new google.visualization.DataTable();
-    data.addColumn('number', 'x');
-    data.addColumn('number', metricName);
-    data.addColumn( {'type': 'string', 'role': 'style'} );
-    data.addColumn( {'type': 'string', 'role': 'annotation'} );
+    const data = {
+      cols: [
+        { label: 'time', type: 'number' },
+        { label: 'metric', type: 'number' },
+        { label: 'point-color', type: 'string', p: { role: 'style' } },
+        { label: 'min-max', type: 'string', p: { role: 'annotation' } },
+        { label: 'min-max-desc', type: 'string', p: { role: 'annotationText' } }
+      ],
+      rows: []
+    };
 
-    var minFound = false;
-    var maxFound = false;
+    let minFound = false;
+    let maxFound = false;
+    let color;
+    let annotation;
+    let annotationText;
 
-    for(let metric of metricsManager.metrics) {
+    for(const metric of metricsManager.metrics) {
       if(metric.value >= thresholds.minimum && metric.value <= thresholds.maximum) {
-        var color = '#ccff00';
+        color = '#ccff00';
       }
       else {
-        var color = '#ff3f00';
+        color = '#ff3f00';
       }
 
-      var annotation = null;
-      if(!minFound && metric.value == metricsManager.statistics.find(x => x.name == 'minimum')) {
+      if(!minFound && metric.value == metricsManager.statistics.find(x => x.name == 'minimum').value) {
         annotation = 'min';
+        annotationText = 'Minimum value';
         minFound = true;
       }
-      else if(!maxFound && metric.value == metricsManager.statistics.find(x => x.name = 'maximum')) {
+      else if(!maxFound && metric.value == metricsManager.statistics.find(x => x.name = 'maximum').value) {
         annotation = 'max';
+        annotationText = 'Maximum value';
         maxFound = true;
       }
-      data.addRow([metric.recordTime, metric.value, `{fill-color: ${color}}`, annotation]);
+
+      const row = {
+        c: [
+          { v: metric.recordTime },
+          { v: metric.value },
+          { v: `{ fill-color: ${color} }`},
+          { v: annotation },
+          { v: annotationText }
+        ]
+      };
+      data.rows.push(row);
     }
 
-    var chart = new google.visualization.LineChart(document.getElementById(chartName));
-    chart.draw(data, null);
+    const googleData = new google.visualization.DataTable(data);
+
+    const options = {
+      vAxis: {
+        title: metricName,
+        titleTextStyle: { color: '#c3dc3c', italic: false },
+        baselineColor: '#c3dc3c',
+        gridlines: { color: '#919191' },
+        textStyle: { color: '#919191' }
+      },
+      hAxis: {
+        title: 'Seconds',
+        titleTextStyle: { color: '#c3dc3c', italic: false },
+        baselineColor: '#c3dc3c',
+        gridlines: { color: '#4b4a4a' },
+        textStyle: { color: '#919191'}
+      },
+      colors: ['#ffdc32'],
+      pointsVisible: 'true',
+      chartArea: { left: '5%', top: '10%', width: '90%', height: '80%' },
+      height: 280,
+      backgroundColor: '#4b4a4a',
+    };
+
+    const chart = new google.visualization.LineChart(document.getElementById(chartName));
+    chart.draw(googleData, options);
   }
 }
