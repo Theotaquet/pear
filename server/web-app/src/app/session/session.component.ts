9a8b4abd66d9c093880f86d34412297a6a334bde
Théo Constant <to.constant@gmail.com>
2018-06-11 20:39:05 +0200

    use chart.js

--- server/web-app/src/app/session/session.component.ts
+++ server/web-app/src/app/session/session.component.ts
@@ -1,18 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Chart } from 'chart.js';
 
 import { SessionService } from '../session.service';
 import { FormatMetricsManagerNamePipe } from '../format-metrics-manager-name.pipe';
 import { FormatChartNamePipe } from '../format-chart-name.pipe';
-
-declare var google: any;
 
 @Component({
   selector: 'app-session',
   templateUrl: './session.component.html',
   styleUrls: ['./session.component.css']
 })
-export class SessionComponent implements OnInit {
+export class SessionComponent implements OnInit, AfterViewInit {
 
   session: any;
 
@@ -27,100 +26,94 @@ export class SessionComponent implements OnInit {
     this.sessionService.getSession(this.route.snapshot.params['sessionId'])
         .subscribe(session => {
           this.session = session;
-          this.loadCharts();
         }
     );
+  }
+
+  ngAfterViewInit() {
+    this.loadCharts();
   }
 
   loadCharts() {
     for(const metricsManager of this.session.metricsManagers) {
       for(const statistic of metricsManager.statistics) {
         if(statistic.thresholds) {
-          google.charts.load('current', { packages: ['corechart'] });
-          google.charts.setOnLoadCallback(() => this.createChart(metricsManager, statistic));
+          this.createChart(metricsManager, statistic);
         }
       }
     }
   }
 
-  createChart(metricsManager, statistic): any {
+  createChart(metricsManager, statistic) {
     const thresholds = statistic.thresholds;
     const metricName = this.formatMetricsManagerNamePipe.transform(metricsManager.name);
     const chartName = this.formatChartNamePipe.transform(metricsManager.name, statistic.name);
 
-    const data = {
-      cols: [
-        { label: 'time', type: 'number' },
-        { label: 'metric', type: 'number' },
-        { label: 'point-color', type: 'string', p: { role: 'style' } },
-        { label: 'min-max', type: 'string', p: { role: 'annotation' } },
-        { label: 'min-max-desc', type: 'string', p: { role: 'annotationText' } }
-      ],
-      rows: []
-    };
-
-    let minFound = false;
-    let maxFound = false;
-    let color;
-    let annotation;
-    let annotationText;
-
+    let data = [];
+    let labels = [];
+    let pointBackgroundColor = [];
     for(const metric of metricsManager.metrics) {
+      data.push(metric.value);
+      labels.push(metric.recordTime);
+
       if(metric.value >= thresholds.minimum && metric.value <= thresholds.maximum) {
-        color = '#ccff00';
+        pointBackgroundColor.push('#ccff00');
       }
       else {
-        color = '#ff3f00';
+        pointBackgroundColor.push('#ff3f00');
       }
-
-      if(!minFound && metric.value == metricsManager.statistics.find(x => x.name == 'minimum').value) {
-        annotation = 'min';
-        annotationText = 'Minimum value';
-        minFound = true;
-      }
-      else if(!maxFound && metric.value == metricsManager.statistics.find(x => x.name = 'maximum').value) {
-        annotation = 'max';
-        annotationText = 'Maximum value';
-        maxFound = true;
-      }
-
-      const row = {
-        c: [
-          { v: metric.recordTime },
-          { v: metric.value },
-          { v: `{ fill-color: ${color} }`},
-          { v: annotation },
-          { v: annotationText }
-        ]
-      };
-      data.rows.push(row);
     }
 
-    const googleData = new google.visualization.DataTable(data);
-
-    const options = {
-      vAxis: {
-        title: metricName,
-        titleTextStyle: { color: '#c3dc3c', italic: false },
-        baselineColor: '#c3dc3c',
-        gridlines: { color: '#919191' },
-        textStyle: { color: '#919191' }
+    const content = {
+      type: 'line',
+      data: {
+        labels: [],
+        datasets: [{
+          data: [],
+          pointBackgroundColor: [],
+          pointBorderWidth: 0.5,
+          pointHoverBorderWidth: 1.5,
+          borderColor: '#ffdc32',
+          lineTension: 0.15,
+          backgroundColor: 'rgba(0, 0, 0, 0.20)'
+        }]
       },
-      hAxis: {
-        title: 'Seconds',
-        titleTextStyle: { color: '#c3dc3c', italic: false },
-        baselineColor: '#c3dc3c',
-        gridlines: { color: '#4b4a4a' },
-        textStyle: { color: '#919191'}
-      },
-      colors: ['#ffdc32'],
-      pointsVisible: 'true',
-      chartArea: { left: '5%', top: '10%', width: '90%', height: '80%' },
-      height: 280,
-      backgroundColor: '#4b4a4a',
+      options: {
+        legend: {
+          display: false
+        },
+        scales: {
+          yAxes: [{
+            scaleLabel: {
+              display: true,
+              labelString: metricName,
+              fontColor: '#c3dc3c'
+            },
+            gridLines: {
+              color: '#606060',
+            },
+            ticks: {
+              fontColor: '#919191'
+            }
+          }],
+          xAxes: [{
+            scaleLabel: {
+              display: true,
+              labelString: 'Seconds',
+              fontColor: '#c3dc3c'
+            },
+            gridLines: {
+              display: false,
+            },
+            ticks: {
+              fontColor: '#919191'
+            }
+          }]
+        }
+      }
     };
 
-    const chart = new google.visualization.LineChart(document.getElementById(chartName));
-    chart.draw(googleData, options);
+    var canvas = <HTMLCanvasElement> document.getElementById(chartName);
+    const chart = new Chart(canvas, content);
   }
 }
